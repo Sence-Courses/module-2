a5542c1af3de8f03eb5cb3c9fe4e7cf5af31baee
JulianSence <julian.sence@gmail.com>
2026-03-14 19:04:15 -0300

    FEATURE: Deposit functionality

--- assets/scripts/listeners.js
+++ assets/scripts/listeners.js
@@ -3,22 +3,11 @@ document.addEventListener('DOMContentLoaded', e => {
   const pageId = pageTitle.split(" ").pop();
   
   switch(pageId) {
-    case 'Principal':
-      addMenuListener();
-      break;
     case 'Depositar':
       addDepositListener();
       break;
-    case 'Dinero':
-      addSendListener();
-      break;
-    case 'Movimientos':
-      addTrxlistener();
-      break;
   }
 });
-
-const addMenuListener = () => {}
 
 const addDepositListener = () => {
   const depositForm = document.getElementById("depositForm");
@@ -48,82 +37,4 @@ const addDepositListener = () => {
     `);
     window.sessionStorage.setItem('saldo', newBalance);
   });
-}
-
-const addSendListener = () => {
-  const listItems = document.querySelectorAll('#contactList li');
-
-  listItems.forEach(item => {
-    item.addEventListener('mouseenter', e => {
-      window.sessionStorage.setItem('idSelContact', item.id);
-    });
-  });
-
-  const contactModal = document.getElementById('contactModal');
-
-  contactModal.addEventListener('shown.bs.modal', e => {
-    const contactForm = document.getElementById('contactForm');
-    const contactList = document.getElementById('contactList');
-
-    contactForm.addEventListener('submit', e => {
-      e.preventDefault();
-      formData = new FormData(e.target);
-      
-      const newContact = {
-        id: Math.floor(Math.random() * 10000) + 1,
-        name: formData.get('nombre'),
-        cbu: formData.get('cbu'),
-        alias: formData.get('alias'),
-        bank: formData.get('banco')
-      }
-      addContact(newContact);
-      window.alert(`Nuevo contacto ${newContact.name} agregado correctamente.`);
-
-      contactList.replaceChildren();
-      contactForm.reset();
-      modal = bootstrap.Modal.getInstance(contactModal);
-      modal.hide();
-      chargeSendData();
-      addSendListener();  
-    });
-  });
-
-  const sendMoneyModal = document.getElementById('sendMoneyModal');
-
-  sendMoneyModal.addEventListener('shown.bs.modal', e => {
-    const contactId = window.sessionStorage.getItem('idSelContact');
-    const contact = findContact(Number(contactId));
-    sendMoneyModal.querySelector('#nombre').value = contact.name;
-    sendMoneyModal.querySelector('#cbu').value = contact.cbu;
-    sendMoneyModal.querySelector('#banco').value = contact.bank;
-
-    const sendMoneyButton = document.getElementById('sendMoneyButton');
-
-    sendMoneyButton.addEventListener('click', e => {
-      const balance = Number(window.sessionStorage.getItem('saldo'));
-      const depositAmount = Number(sendMoneyModal.querySelector('#depositAmount').value);
-      const newBalance = balance - depositAmount;
-      const fDepositAmount = amountFormatter.format(depositAmount);
-      const fNewBalance = amountFormatter.format(newBalance);
-      window.sessionStorage.setItem('saldo', String(newBalance));
-
-      const trx = {
-        type: 'trx-rea',
-        detail: 'Transferencia realizada.',
-        amount: depositAmount
-      }
-      addTrx(trx);
-      
-      window.alert(`
-        Envio realizado con exito
-        -------------------------
-        Monto depositado: ${fDepositAmount}
-        saldo actual:     ${fNewBalance}
-      `);
-    });
-  });
-}
-
-const addTrxlistener = () => {
-  const listItems = document.querySelectorAll('#contactList li');
 }
--- data/charge_data.js
+++ data/charge_data.js
@@ -1,20 +1,3 @@
-let contactos = [
-  {
-    id: 8845,
-    name: 'John Doe',
-    cbu: 123456789,
-    alias: ' john.doe',
-    bank: 'ABC Bank'
-  },
-  {
-    id: 2407,
-    name: 'Jane Smith',
-    cbu: 987654321,
-    alias: 'jane.smith',
-    bank: 'XYZ Bank'
-  }
-];
-
 const transacciones = [
   {
     type: 'buy',
@@ -53,104 +36,20 @@ document.addEventListener('DOMContentLoaded', e => {
   const pageId = pageTitle.split(" ").pop();
   
   switch(pageId) {
-    case 'Login':
-      chargeLoginData();
-      break;
-    case 'Principal':
-      chargeMenuData();
-      break;
     case 'Depositar':
       chargeDepositData();
-      break;
-    case 'Dinero':
-      chargeSendData();
-      break;
-    case 'Movimientos':
-      chargeTransactions();
       break;
   }
 });
 
-const chargeLoginData = () => {
-  window.sessionStorage.setItem('nombreUsuario', 'Peter Jackson');
-  window.sessionStorage.setItem('saldo', '60000');
-  window.sessionStorage.setItem('contactos', JSON.stringify(contactos));
-  window.sessionStorage.setItem('transacciones', JSON.stringify(transacciones));
-}
-
-const chargeMenuData = () => {
-  const balanceElement = document.getElementById("balance");
-  const balance = window.sessionStorage.getItem('saldo');
-  balanceElement.textContent = amountFormatter.format(balance);
-}
-
 const chargeDepositData = () => {
+  if (window.sessionStorage.getItem('saldo') == null) {
+    window.sessionStorage.setItem('saldo', '60000');
+  }  
+  window.sessionStorage.setItem('transacciones', JSON.stringify(transacciones));
   const balanceElement = document.getElementById("balance");
   const balance = window.sessionStorage.getItem('saldo');
   balanceElement.textContent = amountFormatter.format(balance);
-}
-
-const chargeSendData = () => {
-  const getContacts = () => window.sessionStorage.getItem('contactos');
-  const contactList = document.getElementById('contactList');
-  const contacts = JSON.parse(getContacts());
-
-  contacts.forEach( e => {
-    const li = document.createElement('li');
-    li.id = e.id;
-    const div = document.createElement('div');
-    div.classList.add('contact-info','label-alone');
-    const name = document.createElement('label');
-    name.classList.add('contact-name');
-    name.textContent = 'Nombre: ' + e.name;
-    const cbu = document.createElement('label');
-    cbu.classList.add('contact-details');
-    cbu.textContent = 'CBU: ' + e.cbu;
-    const alias = document.createElement('label');
-    alias.classList.add('contact-details');
-    alias.textContent = 'Alias: ' + e.alias;
-    const banco = document.createElement('label');
-    banco.classList.add('contact-details');
-    banco.textContent = 'Banco: ' + e.bank;
-    div.append(name, cbu, alias, banco);
-    li.appendChild(div);
-    contactList.appendChild(li);
-  });
-  const button = document.createElement('button');
-  button.type = 'button';
-  button.id = 'openSendMoneyModal';
-  button.classList.add('btn','btn-primary','button');
-  button.setAttribute('data-bs-toggle', 'modal');
-  button.setAttribute('data-bs-target', '#sendMoneyModal');
-  button.textContent = 'Enviar dinero';
-  contactList.appendChild(button);
-}
-
-const chargeTransactions = () => {
-  const getTrxs = () => window.sessionStorage.getItem('transacciones');
-  const trxList = document.getElementById('trxList');
-  const trxs = JSON.parse(getTrxs());
-
-  trxs.forEach( e => {
-    const li = document.createElement('li');
-    const div = document.createElement('div');
-    div.classList.add('trx-li-style');
-    const iconElement = document.createElement('i');
-    
-    if (e.type === 'deposit' || e.type === 'trx-rec') {
-      iconElement.className = 'fa-solid fa-hand-holding-dollar';
-    }
-    else {
-      iconElement.className = 'fa-solid fa-money-bill-trend-up';
-    }    
-    const detalle = document.createElement('label');
-    const amount = amountFormatter.format(e.amount);
-    detalle.textContent = `Detalle: ${e.detail} Monto: ${amount}`;
-
-    div.append(iconElement, detalle);
-    li.appendChild(div);
-    trxList.appendChild(li);
-  });
 }
 
 const amountFormatter = new Intl.NumberFormat('es-CL', {
